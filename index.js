2838b9859e8baa56ee7da76f57e9133b5c9433c5
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-06-05 09:43:28 +0200

    style: apply eslint suggestions

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const express = require('express')
- 
+const express = require('express');
+
 const token = process.env.TELEGRAM_TOKEN;
 let bot;
 
 if (process.env.NODE_ENV === 'production') {
-   bot = new TelegramBot(token);
-   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 } else {
-   bot = new TelegramBot(token, { polling: true });
+  bot = new TelegramBot(token, {polling: true});
 }
 
-console.log(token)
+console.log(token);
 
 bot.onText(/.*/, (msg, _) => {
-    console.log('received message: ', msg)
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Non ora signor giudice, sto giocando ai videogames!", { parse_mode: 'HTML' });
+  console.log('received message: ', msg);
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId,
+      'Non ora signor giudice, sto giocando ai videogames!',
+      {parse_mode: 'HTML'});
 });
 
 bot.on('message', (msg) => {
-   console.log('on message', msg)
-   const chatId = msg.chat.id
-   bot.sendMessage(chatId, 'Mi legano le mani, ma io scrivo con l\'uccello')
-})
+  console.log('on message', msg)
+  const chatId = msg.chat.id
+  bot.sendMessage(chatId, 'Mi legano le mani, ma io scrivo con l\'uccello')
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.listen(process.env.PORT);
- 
+
 app.post('/' + bot.token, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
